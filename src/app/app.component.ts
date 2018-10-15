d83fca3726f7342c6428b419b80ed981a5b8ae79
b3nnylava <b3nnylava@horsefucker.org>
2018-10-15 00:00:22 -0500

    removed routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,21 @@ import DxThemes from 'devextreme/ui/themes'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isCard
+
   ngOnInit () {
     DxThemes.current('generic.dark')
+  }
+
+  onOptionChanged (e) {
+    if (e.value) {
+      DxThemes.current('generic.dark')
+      $('body').css('background-color','#424548')
+      $('#menu').removeClass('navbar-light bg-light').addClass('navbar-dark bg-dark')
+    } else {
+      DxThemes.current('generic.light')
+      $('body').css('background-color','lightgrey')
+      $('#menu').removeClass('navbar-dark bg-dark').addClass('navbar-light bg-light')
+    }
   }
 }
