7cae5d329f15ef697e3ea1e96e37b90bca248e83
b3nnylava <b3nnylava@horsefucker.org>
2018-10-26 13:39:44 -0500

    working on collapse functionality

--- src/app/services/data/editions.ts
+++ src/app/services/data/editions.ts
@@ -8,4 +8,12 @@ export const editions: Edition[] = [{
   status: 'On Demand',
   metaTags: ['Hardcover'],
   notes: 'some fake entry notes'
+},{
+  id: 2,
+  bookId: 1,
+  imageLink: 'http://static.lulu.com/browse/product_thumbnail.php?productId=16671951&resolution=320',
+  storeLink: 'http://www.lulu.com/shop/jonathan-lopez/doctor-whoof-the-complete-series-directors-cut/hardcover/product-16671951.html',
+  status: 'On Demand',
+  metaTags: ['Hardcover'],
+  notes: 'some fake entry notes'
 }]
